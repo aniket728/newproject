43c0ff5631046b626b06254a56d999a8331979a9
aniket728 <aniketkale728@gmail.com>
2025-01-31 14:38:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Dashboard from './components/vendorpanel/layouts/Dashboard';
 import Vendorregister from './components/vendorpanel/Vendorregister';
 import Vendorlogin from './components/vendorpanel/Vendorlogin';
 import Thankyou from './components/vendorpanel/Thankyou';
+import CreditNotes from './components/vendorpanel/CreditNotes';
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
     <Route path='/' Component={Layout}>
-    <Route index Component={Home}/>
-</Route>
-<Route path='/dashboard' Component={Dashboard}/>
-<Route path='/vendorregister' Component={Vendorregister}/>
+    <Route path='/vendorregister' Component={Vendorregister}/>
 <Route path='/vendorlogin' Component={Vendorlogin}/>
 <Route path='thank' Component={Thankyou}/>
+</Route>
+<Route path='/dashboard' Component={Dashboard}/>
+
+
+
    </Routes>
    
    </BrowserRouter>
---
+++ src/components/vendorpanel/CreditNotes.js
@@ -0,0 +1,323 @@
+import React from 'react';
+import Thankyou from './Thankyou';
+import { Link } from 'react-router-dom';
+
+const CreditNotes = () => {
+  return (
+    <>
+    <div className="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
+      {/* Sidebar */}
+      <nav className="navbar show navbar-vertical h-lg-screen navbar-expand-lg px-0 py-3 navbar-light bg-white border-bottom border-bottom-lg-0 border-end-lg" id="navbarVertical">
+        <div className="container-fluid">
+          {/* Sidebar Toggle Button */}
+          <button className="navbar-toggler ms-n2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarCollapse" aria-controls="sidebarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Brand Logo */}
+          <a className="navbar-brand py-lg-2 mb-lg-5 px-lg-6 me-0" href="#">
+            <img src="https://preview.webpixels.io/web/img/logos/clever-primary.svg" alt="..." />
+          </a>
+
+          {/* User Avatar (Mobile) */}
+          <div className="navbar-user d-lg-none">
+            <div className="dropdown">
+              <a href="#" id="sidebarAvatar" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div className="avatar-parent-child">
+                  <img alt="Image Placeholder" src="https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" className="avatar avatar- rounded-circle" />
+                  <span className="avatar-child avatar-badge bg-success"></span>
+                </div>
+              </a>
+              <div className="dropdown-menu dropdown-menu-end" aria-labelledby="sidebarAvatar">
+                <a href="#" className="dropdown-item">Profile</a>
+                <a href="#" className="dropdown-item">Settings</a>
+                <a href="#" className="dropdown-item">Billing</a>
+                <hr className="dropdown-divider" />
+                <a href="#" className="dropdown-item">Logout</a>
+              </div>
+            </div>
+          </div>
+
+          {/* Sidebar Navigation */}
+          <div className="collapse navbar-collapse show" id="sidebarCollapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <i className="bi bi-house"></i> Dashboard
+                </a>
+              </li>
+
+              {/* Inventory Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#inventoryCollapse" aria-expanded="false" aria-controls="inventoryCollapse">
+                  <i className="bi bi-box"></i> Inventory
+                </a>
+                <div className="collapse" id="inventoryCollapse">
+                  <ul className="nav flex-column ms-3">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Items</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Warehouses</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Price Lists</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              {/* Routes Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#routesCollapse" aria-expanded="false" aria-controls="routesCollapse">
+                  <i className="bi bi-map"></i> Routes
+                </a>
+                <div className="collapse" id="routesCollapse">
+                  <ul className="nav flex-column ms-3">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Regions</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Cities</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Areas</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Attendance</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              {/* Report Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <i className="bi bi-file-earmark-text"></i> Report
+                </a>
+              </li>
+
+              {/* Settings Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <i className="bi bi-gear"></i> Settings
+                </a>
+              </li>
+
+              {/* User Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#userCollapse" aria-expanded="false" aria-controls="userCollapse">
+                  <i className="bi bi-person"></i> User
+                </a>
+                <div className="collapse" id="userCollapse">
+                  <ul className="nav flex-column ms-3">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">User</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Target</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Leaderboard</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Achievements</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              {/* Sales Section */}
+              <li className="nav-item">
+                <Link className="nav-link" to="./" data-bs-toggle="collapse" data-bs-target="#salesCollapse" aria-expanded="false" aria-controls="salesCollapse">
+                  <i className="bi bi-cart"></i> Sales
+                </Link>
+                <div className="collapse" id="salesCollapse">
+                  <ul className="nav flex-column ms-3">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Estimates</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Sales Order</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Delivery Challan</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Sales Return</a>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="">Credit Notes</Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              {/* Purchase Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#purchaseCollapse" aria-expanded="false" aria-controls="purchaseCollapse">
+                  <i className="bi bi-bag"></i> Purchase
+                </a>
+                <div className="collapse" id="purchaseCollapse">
+                  <ul className="nav flex-column ms-3">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Purchase Order</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Purchase Invoice</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Purchase Return</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              {/* Van Sales */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <i className="bi bi-truck"></i> Van Sales
+                </a>
+              </li>
+
+              {/* Live Location */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <i className="bi bi-map"></i> Live Location
+                </a>
+              </li>
+
+              {/* Parties Section */}
+              <li className="nav-item">
+                <a className="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#partiesCollapse" aria-expanded="false" aria-controls="partiesCollapse">
+                  <i className="bi bi-people"></i> Parties
+                </a>
+                <div className="collapse" id="partiesCollapse">
+                  <ul className="nav flex-column ms-3">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Customers</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Suppliers</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Groups</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Visited</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+
+            <hr className="navbar-divider my-5 opacity-20" />
+
+            <div className="mt-auto"></div>
+          </div>
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <div className="h-screen flex-grow-1 overflow-y-lg-auto">
+        {/* Top Navbar */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+          <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavbar" aria-controls="topNavbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="topNavbar">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+              </ul>
+
+              <form className="d-flex">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" alt="User" className="rounded-circle" width="30" height="30" />
+                  </a>
+                  <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a className="dropdown-item" href="#">Profile</a></li>
+                    <li><a className="dropdown-item" href="#">Settings</a></li>
+                    <li><a className="dropdown-item" href="#">Billing</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Logout</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        {/* Header and Main Content */}
+        <header className="bg-surface-primary border-bottom pt-6">
+          <div className="container-fluid">
+            <div className="mb-npx">
+              <div className="row align-items-center">
+                <div className="col-sm-6 col-12 mb-4 mb-sm-0">
+                  <h1 className="h2 mb-0 ls-tight">Application</h1>
+                </div>
+                <div className="col-sm-6 col-12 text-sm-end">
+                  <div className="mx-n1">
+                    <a href="#" className="btn d-inline-flex btn-sm btn-neutral border-base mx-1">
+                      <span className="pe-2">
+                        <i className="bi bi-pencil"></i>
+                      </span>
+                      <span>Edit</span>
+                    </a>
+                    <a href="#" className="btn d-inline-flex btn-sm btn-primary mx-1">
+                      <span className="pe-2">
+                        <i className="bi bi-plus"></i>
+                      </span>
+                      <span>Create</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <ul className="nav nav-tabs mt-4 overflow-x border-0">
+                <li className="nav-item">
+                  <a href="#" className="nav-link active">All files</a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link font-regular">Shared</a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link font-regular">File requests</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </header>
+
+        <main className="py-6 bg-surface-secondary">
+          <div className="container-fluid">
+         
+     <Thankyou/>
+         
+          </div>
+        </main>
+      </div>
+    </div>
+  </>
+  );
+};
+
+export default CreditNotes;
